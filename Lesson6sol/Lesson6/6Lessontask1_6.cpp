989e96f88e18c405832446e6795b59d3b1bc4a55
AzizbekTursunov <tursunov.ag@phystech.edu>
2022-03-14 04:43:29 +0300

    Added Lesson6-7

---
+++ Lesson6sol/Lesson6/6Lessontask1_6.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "russian");
+	std::cout << "Введите целое положительное число!" << std::endl;
+	int num;
+	int sum = 0;
+	int i = 0;
+	std::cin >> num;
+	while (true) {
+		if (num >= 0) break;
+		else
+		{
+			std::cout << "Введите целое положительное число!" << std::endl;
+			std::cin >> num;
+		}
+	}
+	while (i < num) {
+		sum += i;
+		i++;
+	}
+	std::cout << "Сумма чисел от 1 до "<< num << " равно: " << sum << std::endl;
+
+	int massiv[] = { 45, 2, -6, 8, 78, 15, -62, 23, 12, 99 };
+	int sum_m = 0;
+	for (i = 0; i < 10; i++) {
+		 // if (i % 2 == 1) { std::cout << massiv[i] << "\n"; }
+		if (i % 2 == 0) {sum_m += massiv[i];}
+
+	}
+	std::cout << "Cумма элементов массива, стоящих на нечётных позициях равно : " << sum_m << "\n";
+
+
+}
